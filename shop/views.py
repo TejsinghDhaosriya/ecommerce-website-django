e05831949056dbdebaabc3fb0b8c96cdf556f847
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-03-16 11:48:45 +0530

    fetching products from databaSe

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
+from math import ceil
 # Create your views here.
 def index(request):
-    return render(request,'shop/index.html')
+    products=(Product.objects.all())
+    n=len(products)
+    nSlides=n//4 + ceil((n/4) -(n//4))
+    params={'no_of_slides':nSlides,'range':range(1,nSlides),'product':products}
+    return render(request,'shop/index.html',params)
 
     #return HttpResponse("myblog") 
 def tacker(request):
