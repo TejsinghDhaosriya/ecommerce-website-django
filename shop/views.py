de6d120c12313ecada7378de5a0b348925d4ee65
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-03-16 15:35:36 +0530

    added item based on category

--- shop/views.py
+++ shop/views.py
@@ -4,14 +4,25 @@ from .models import Product
 from math import ceil
 # Create your views here.
 def index(request):
+
     products=(Product.objects.all())
-    n=len(products)
-    nSlides=n//4 + ceil((n/4) -(n//4))
+    #n=len(products)
+    #nSlides=n//4 + ceil((n/4) -(n//4))
     #params={'no_of_slides':nSlides,'range':range(1,nSlides),'product':products}
-    allProds=[[products,range(1,nSlides),nSlides],
-              [products,range(1,nSlides),nSlides]]
-    params={'allProds':allProds}
-    return render(request,'shop/index.html',params)
+    # allProds=[[products,range(1,nSlides),nSlides],
+    #           [products,range(1,nSlides),nSlides]]
+    
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
     #return HttpResponse("myblog") 
 def tacker(request):
