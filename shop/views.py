ae88e9154f9fb31cc005fca589aae18364a5d5a3
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-03-18 23:33:04 +0530

    creating contact us

--- shop/views.py
+++ shop/views.py
@@ -31,6 +31,8 @@ def search(request):
     return render(request,'shop/search.html')
 
 def contact(request):
+    if request="POST":
+        print(request)
     return render(request,'shop/contact.html')
 
 def productview(request):
